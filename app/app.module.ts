52dccb2fa95791514009bdb97a9b151f8e1fd9af
Matt Huddleston <mthuddleston@gmail.com>
2016-10-13 09:26:40 -0400

    End of part 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,55 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }        from './app.component';
-import {HeroesComponent} from './heroes.component';
-import {HeroDetailComponent} from "./hero-detail.component";
-import { HeroService }         from './hero.service';
-import {DashboardComponent} from "./dashboard.component";
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
 
+//Routes imported here
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-   imports: [
-       BrowserModule,
-       FormsModule,
-       RouterModule.forRoot([
-           {
-               path: 'heroes',
-               component: HeroesComponent
-           },
-           {
-               path: 'dashboard',
-               component: DashboardComponent
-           },
-           {
-               path: '',
-               redirectTo: '/dashboard',
-               pathMatch: 'full'
-           },
-           {
-               path: 'detail/:id',
-               component: HeroDetailComponent
-           },
-           {
-               path: 'detail/:id',
-               component: HeroDetailComponent
-           }
-       ])
-   ],
-   declarations: [
-       AppComponent,
-       HeroesComponent,
-       HeroDetailComponent,
-       DashboardComponent
-   ],
-    providers: [
-        HeroService
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
     ],
-   bootstrap:    [
-       AppComponent
-   ]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
 })
 
-export class AppModule {}
+export class AppModule { }
